fec067e2cc28cd3e8c7cabb3bc2a75d6d3eb6d99
sgkim6326 <dirtyfly58@gmail.com>
2021-03-20 14:03:47 +0900

    1초 마다 시간 증가 업데이트

--- OPC_UA_Sample_Server.py
+++ OPC_UA_Sample_Server.py
@@ -1,20 +1,37 @@
 
 from opcua import ua, Server as OPC_UA_Server
+import threading
+    
+class Sampe_Server:
+    def __init__(self):
+        self.i = 0.1
+        self.server = OPC_UA_Server()
+        self.server.set_endpoint("opc.tcp://localhost:4840/sampleopcuaserver/")
+        uri = "machine.sample1"
+        idx = self.server.register_namespace(uri)
+        objects = self.server.get_objects_node()
+        myobj = objects.add_object(idx, "Machine_Sample_1")
+        self.obj = myobj.add_variable(idx, "Sample Double Value", 0.1)
+        myobj.add_variable(idx, "Sample String Value", "Sample 1")
+        uri = "machine.sample2"
+        idx = self.server.register_namespace(uri)
+        objects = self.server.get_objects_node()
+        myobj = objects.add_object(idx, "Machine Sample")
+        myobj.add_variable(idx, "Sample Double Value", 0.1)
+        myobj.add_variable(idx, "Sample String Value", "Sample 2")
+        self.server.start()
+    def Start(self):
+        self.event = threading.Timer(1, self.Change_value)
+        self.event.deamon = True
+        self.event.start()
+    def Change_value(self):
+        self.i = self.i + 0.1
+        if self.i> 10000:
+            self.i = 0.0
+        self.obj.set_value(self.i)
+        self.Start()
 
+import OPC_UA_Sample_Server
 if __name__ == "__main__":
-    server = OPC_UA_Server()
-    server.set_endpoint("opc.tcp://localhost:4840/sampleopcuaserver/")
-    uri = "plc.sample1"
-    idx = server.register_namespace(uri)
-    objects = server.get_objects_node()
-    myobj = objects.add_object(idx, "PLC_Sample_1")
-    myobj.add_variable(idx, "Sample Double Value", 0.1)
-    myobj.add_variable(idx, "Sample String Value", "Sample 1")
-
-    uri = "plc.sample2"
-    idx = server.register_namespace(uri)
-    objects = server.get_objects_node()
-    myobj = objects.add_object(idx, "PLC_Sample_2")
-    myobj.add_variable(idx, "Sample Double Value", 0.1)
-    myobj.add_variable(idx, "Sample String Value", "Sample 2")
-    server.start()
+    server = Sampe_Server()
+    server.Start()
